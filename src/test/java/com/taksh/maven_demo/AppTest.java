db0e9529afda309d1259e6890cd0ad5989b8c860
Saksham Singh <sakshamx@Sakshams-MacBook-Air.local>
2026-01-03 11:42:11 +0530

    first commit

---
+++ src/test/java/com/taksh/maven_demo/AppTest.java
@@ -0,0 +1,13 @@
+package com.taksh.maven_demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
